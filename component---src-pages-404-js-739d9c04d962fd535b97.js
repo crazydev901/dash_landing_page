1da3e8bb2e9d5e41306be2b2454ad7a516798582
crazydev901 <david90192@outlook.com>
2021-02-15 09:42:06 -0500

    Updates

---
+++ component---src-pages-404-js-739d9c04d962fd535b97.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),l=n.n(t);a.default=function(){return l.a.createElement("div",{id:"main"},l.a.createElement("div",{className:"inner"},l.a.createElement("header",{className:"major"},l.a.createElement("h2",null,"NOT FOUND")),l.a.createElement("section",null,l.a.createElement("h4",null,"Not a valid URL"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-739d9c04d962fd535b97.js.map
--- src/content/code_snippet_1.md
+++
@@ -1,6 +0,0 @@
-What you'll learn
-How to set up a JavaScript project which is able to interact with the Dash network.
-How to create a Dash wallet.
-How to use the evonet faucet.
-How to check the balance of your wallet.
-How to send funds.
